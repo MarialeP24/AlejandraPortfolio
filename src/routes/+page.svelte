a462c6a29b35c0c17f8b88e0c2586dcb76daee3f
Maria Pardini Osorio <mariale.pardini@gmail.com>
2025-03-06 05:46:45 -0500

    lab4

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1> Alejandra</h1>
+
+<svelte:head>
+  <title>About me</title>
+</svelte:head>
+   
+<p>Hello! My name is Maria Alejandra , I have a background in architecture and design, I have spent over six years bridging public policy and spatial design, focusing on community-driven and impactful projects. My work at the Ministry of Education in Peru involved leading nationwide school infrastructure initiatives, emphasizing innovative solutions for rural areas. Later, at El Equipo Mazzanti, I contributed to research-driven projects that reimagine public spaces. Now, as a Master in Design Studies candidate at Harvard GSD, I explore the intersections of policy, public space, and climate-responsive urban design, aiming to shape inclusive and resilient environments through participatory and data-driven strategies. </p>
+<img src = "Images /Mota.jpeg" alt = "Merry Xmas from Mota">
