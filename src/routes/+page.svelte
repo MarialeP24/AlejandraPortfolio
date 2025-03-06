17a609598b4dbfee4e8fcfcab49a86fb02347672
Maria Pardini Osorio <mariale.pardini@gmail.com>
2025-03-06 10:08:08 -0500

    Try

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script>
+    import projects from "$lib/projects.json";
+    import Project from "$lib/Project.svelte";
+    </script>
+
 <h1> Alejandra</h1>
 
 <svelte:head>
@@ -5,4 +10,13 @@
 </svelte:head>
    
 <p>Hello! My name is Maria Alejandra , I have a background in architecture and design, I have spent over six years bridging public policy and spatial design, focusing on community-driven and impactful projects. My work at the Ministry of Education in Peru involved leading nationwide school infrastructure initiatives, emphasizing innovative solutions for rural areas. Later, at El Equipo Mazzanti, I contributed to research-driven projects that reimagine public spaces. Now, as a Master in Design Studies candidate at Harvard GSD, I explore the intersections of policy, public space, and climate-responsive urban design, aiming to shape inclusive and resilient environments through participatory and data-driven strategies. </p>
+
 <img src = "Images /Mota.jpeg" alt = "Merry Xmas from Mota">
+
+<h2>Latest Projects</h2>
+
+<div class="projects">
+    {#each projects.slice(0, 3) as p}
+    <Project data={p} />
+    {/each}
+</div>
